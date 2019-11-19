be32bf3b4ec23de34a19fc7b4fa2852cb4582dc5
Vonds <vonds13@gmail.com>
2019-11-19 10:26:05 -0500

    sock merchant 2

--- hackerRank/sockMerchant/app.js
+++ hackerRank/sockMerchant/app.js
@@ -1,8 +1,11 @@
+// 1
 const sockMerchant = (arr, match = {}) => {
   let counter = 0;
-  for (let sock of arr) {
+  arr.forEach(sock => {
     match[sock] = match[sock] + 1 || 1;
     if (match[sock] % 2 === 0) counter++;
-  }
+  });
   return counter;
 };
+
+console.log(sockMerchant([10, 10, 20, 10, 50, 50, 60]));
