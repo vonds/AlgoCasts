c2b743234193ba6307b75ea0734bcbc506be6200
Vonds <vonds13@gmail.com>
2019-11-19 11:45:36 -0500

    counting valleys

--- hackerRank/countingValleys/app.js
+++ hackerRank/countingValleys/app.js
@@ -1,9 +1,9 @@
-const countingValleys = (n, s) => {
+const countingValleys = (steps, slope) => {
   let up = 0;
   let valleys = 0;
-  for (let i = 0; i < n; i++) {
-    if (s[i] === "D") --up;
-    if (s[i] === "U") ++up === 0 ? valleys++ : null;
+  for (let i = 0; i < steps; i++) {
+    if (slope[i] === "D") --up;
+    if (slope[i] === "U") ++up === 0 ? valleys++ : null;
   }
   return valleys;
 };
