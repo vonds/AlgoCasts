ad2b4fb5c4ebc84bc23838637460d30650c20e86
Vonds <vonds13@gmail.com>
2019-11-19 11:02:59 -0500

    davis' staircase

--- hackerRank/davisStaircase/app.js
+++ hackerRank/davisStaircase/app.js
@@ -1,10 +1,10 @@
-const countSteps = (steps, memory={}) {
-    if (steps < 0) return 0
-    if (steps === 1) return 1
-    if (steps === 2) return 2
-    if (steps === 3) return 4
-    return (memory[steps] = 
-        countSteps(steps - 1, memory) +
-        countSteps(steps - 2, memory) +
-        countSteps(steps - 3, memory))
-}
+const countSteps = (steps, memory = {}) => {
+  if (steps < 0) return 0;
+  if (steps === 1) return 1;
+  if (steps === 2) return 2;
+  if (steps === 3) return 4;
+  return (memory[steps] =
+    countSteps(steps - 1, memory) +
+    countSteps(steps - 2, memory) +
+    countSteps(steps - 3, memory));
+};
