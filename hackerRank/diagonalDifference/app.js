6805a6ff557e50fff3f09bbf699d32d6d451c58e
Vonds <vonds13@gmail.com>
2019-11-21 12:32:06 -0500

    diagonal difference 2

--- hackerRank/diagonalDifference/app.js
+++ hackerRank/diagonalDifference/app.js
@@ -1,14 +1,14 @@
-function diagonalDifference(arr) {
-  const length = arr.length;
+const diagonalDifference = matrix => {
+  const length = matrix.length
   let diagonal1 = 0,
-    diagonal2 = 0;
+  diagonal2 = 0
 
-  for (let i = 0; i < length; i++) {
-    diagonal1 += arr[i][i];
-    diagonal2 += arr[length - 1 - i][i];
+  for(let i = 0; i < length; i++) {
+    diagonal1 += matrix[i][i]
+    diagonal2 += matrix[length - 1 - i][i]
   }
 
-  return Math.abs(diagonal1 - diagonal2);
+  return Math.abs(diagonal1 - diagonal2)
 }
-
-diagonalDifference([11, 2, 4, 4, 5, 6, 10, 8, -12]);
+ 
+console.log(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
