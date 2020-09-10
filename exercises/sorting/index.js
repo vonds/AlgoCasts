2dbf2b4aed5028ef3ab0d3ee47397efd85a2b623
Vonds <vonds13@gmail.com>
2020-09-10 07:36:32 -0400

    selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,70 +1,65 @@
-// --- Directions
-// Implement bubbleSort, selectionSort, and mergeSort
+--- Directions
+Implement bubbleSort, selectionSort, and mergeSort
 
-// function bubbleSort(arr) {
-//     for(let i = 0; i < arr.length; i++) {
-//         for(let j = 0; j < (arr.length - i - 1); j++) {
-//             if(arr[j] > arr[j + 1]) {
-//                 const lesser = arr[j + 1];
-//                 arr[j + 1] = arr[j];
-//                 arr[j] = lesser;
-//             }
-//         }
-//     }
-//     return arr;
-// }
-
-// function selectionSort(arr) {
-
-// }
-
-// function mergeSort(arr) {
-
-// }
-
-// function merge(left, right) {
-
-// }
-
-
-// function reverseStr(str) {
-//      return str.split('').reduce((reversed, character) => {
-//           return character + reversed;
-//       }, '');
-// }
-
-
-
-// module.exports = { bubbleSort, selectionSort, mergeSort, merge };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// create a function that takes in a string and reverses the characters
-
-
-const reverseString6 = str => {
-   let reverse = '';
-   for(let i = str.length - 1; i >= 0; i--) {
-       reverse += str[i];
-   }
-   return reverse;
+function bubbleSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < (arr.length - i - 1); j++) {
+            if(arr[j] > arr[j + 1]) {
+                const lesser = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+    return arr;
 }
 
-// test cases
-console.log(reverseString6('ym sdneirflrig evol taht gnos')); // year up is syrup
-// stringReverse2('purys si pu raey');
-// stringReverse3('purys si pu raey');
-// stringReverse4('purys si pu raey');
-// stringReverse5('purys si pu raey');
-// stringReverse6('purys si pu raey');
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+
+        if(indexOfMin !== i) {
+            let lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+
+    return arr;
+}
+
+function mergeSort(arr) {
+
+}
+
+function merge(left, right) {
+
+}
+
+
+function reverseStr(str) {
+     return str.split('').reduce((reversed, character) => {
+          return character + reversed;
+      }, '');
+}
+
+
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+
+
+
+
+
+
+
+
+
+
+
