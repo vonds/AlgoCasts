269e69e1d4a7fd77a772bad2ca0ba6bf22cf075d
Vonds <vonds13@gmail.com>
2020-09-22 10:00:53 -0400

    notes on graphs

---
+++ interviewPrep/graphs/app.js
@@ -0,0 +1,9 @@
+// A graph is set of values that are related in a pair like relationship
+// each item is called a node or vertex
+// each node is connected by an edge
+// graphs represent networks/connections
+// there are directed and undirected graphs
+// directed graphs go in only one direction // twitter
+// undirected graphs can guy either way // facebook
+// graphs can be weighted or unweighted
+// graphs can be cyclic or acyclic
